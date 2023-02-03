e902aa968c7d50bc55e1bb73469296fc80f898b7
Joao Meyer <1994meyer@gmail.com>
2023-02-03 18:34:51 +0000

    Fixed configurations

--- ucloud/settings.py
+++ ucloud/settings.py
@@ -5,15 +5,15 @@ from pydantic import BaseSettings
 
 class Config(BaseSettings):
     UCLOUD_REST_TYPE: str = 'sqlite'
-    UCLOUD_REST_SQLITE_PATH: str = 'sqlite:////tmp/ucloud'
+    UCLOUD_REST_SQLITE_PATH: str = 'sqlite:////tmp/ucloud_sqlite'
     UCLOUD_REST_MONGODB_PATH: str = 'mongodb://127.0.0.1'
     UCLOUD_REST_POSTGRESQL_PATH: str = 'postgresql://postgres@127.0.0.1/postgres'
 
     UCLOUD_FILES_TYPE: str = 'local'
-    UCLOUD_FILES_LOCAL_PATH: str = '/tmp/ucloud'
+    UCLOUD_FILES_LOCAL_PATH: str = '/tmp/ucloud_files'
 
     UCLOUD_QUEUE_TYPE: str = 'sqlite'
-    UCLOUD_QUEUE_SQLITE_PATH: str = 'sqlite:////tmp/ucloud'
+    UCLOUD_QUEUE_SQLITE_PATH: str = 'sqlite:////tmp/ucloud_sqlite'
     UCLOUD_QUEUE_MONGODB_PATH: str = 'mongodb://127.0.0.1'
     UCLOUD_QUEUE_POSTGRESQL_PATH: str = 'postgresql://postgres@127.0.0.1/postgres'
 
